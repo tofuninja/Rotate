074e33d8a7520d9ad0354e95ccf270fddebd6698
Dongwon <dongwon5969@hotmail.com>
2014-04-29 18:19:31 -0400

    Functions
    with tests

--- cs252 Rotate Game/src/Board.java
+++ cs252 Rotate Game/src/Board.java
@@ -71,6 +71,7 @@ public class Board {
 		}
 		if(Math.random()>0.5)
 			board[0][0].mark=2;
+		
 		if(Math.random()>0.5)
 			end.mark=2;
 	}
@@ -82,13 +83,16 @@ public class Board {
 		//System.out.println("run on function");
 		if(myPoint==null)
 			System.out.println("error");
+		
 		myPoint.mark=1;
+		
 		if(end.mark==1)
 		{
 			return 1;//solution found			
 		}
 		//make an array of 4 points for all directions from current point
 		Point north,south,west,east;
+		
 		if(myPoint.row-1<0)
 			north=null;
 		else
@@ -97,17 +101,20 @@ public class Board {
 		if(myPoint.row+1>=maxRow)
 			south=null;
 		else
-			south=board[myPoint.row+1][myPoint.col]; 
+			south=board[myPoint.row+1][myPoint.col];
+		
 		if(myPoint.col-1<0)
 			west=null;
 		else
 			west=board[myPoint.row][myPoint.col-1];
+		
 		if(myPoint.col+1>=maxCol)
 			east=null;
 		else
 			east=board[myPoint.row][myPoint.col+1];
+		
 		Point []locations={north,south,west,east};
-		//randomize array loactions
+		//randomize array locations
 		if(Math.random()>0.5)
 		{
 			Point temp=locations[0];
@@ -194,4 +201,150 @@ public class Board {
 		return 0;
 	}
 	
+	
+	public int checkIfCorrect(Point [][]board)//should implement a timer later so it's not infinite
+	{
+		
+		if(1==South(board, board[0][0]))//replace with south function
+			return 1;
+		
+		if(1==East(board, board[0][0]))//replace with east function
+			return 1;
+		
+		return 0;
+	}
+	
+	public int South(Point [][]board, Point currentPoint)
+	{
+		//TODO check if exit (a-1,b-1)...so end pt row/col
+		//do this for North West and East too
+		if(currentPoint.row==maxRow && currentPoint.col==maxCol)
+			return 1;		//check current position is (a-1, b-1) which means end point.
+		
+		if (currentPoint.value==1)
+		{
+			//TODO
+			//check if calling point is valid  i.e) currentPoint.row+1 is out of bounds or not
+			//if it is out of bounds then return 0
+			//you have to do this here for every if else that calls another function i.e) South North West East
+			//meaning you should do this for 3*4directions=12times
+			//hint just write code for south call east call north call west call and copy and past for each of the different calls
+			//this way you only have to write 4 different test cases
+			
+			if(currentPoint.row+1>maxRow)
+				return 0;
+
+			return South(board, board[currentPoint.row+1][currentPoint.col]);
+		}
+		
+		else if(currentPoint.value==3)
+		{
+			if(currentPoint.col+1>maxCol)
+				return 0;
+			
+			return East(board, board[currentPoint.row][currentPoint.col+1]);
+			
+		}
+		
+		else if(currentPoint.value==4)
+		{
+			if(currentPoint.col-1<0)
+				return 0;
+			
+			return West(board, board[currentPoint.row][currentPoint.col-1]);
+		}
+		
+		else return 0;
+	}
+	
+	public int North(Point [][]board, Point currentPoint)
+	{
+		if(currentPoint.row==maxRow && currentPoint.col==maxCol)
+			return 1;		//check current position is (a-1, b-1) which means end point.
+		
+		if(currentPoint.value==1)
+		{
+			//check if valid otherwise return 0;
+			if(currentPoint.row-1<0)
+				return 0;			
+
+			return North(board, board[currentPoint.row-1][currentPoint.col]);
+		}
+		else if(currentPoint.value==5)
+		{
+			if(currentPoint.col-1<0)
+				return 0;
+			
+			return West(board, board[currentPoint.row][currentPoint.col-1]);
+		}
+		else if(currentPoint.value==6)
+		{
+			if(currentPoint.col+1>maxCol)
+				return 0;
+			
+			return East(board, board[currentPoint.row][currentPoint.col+1]);
+		}
+		else return 0;
+	}
+	
+	public int West(Point [][]board, Point currentPoint)
+	{
+		if(currentPoint.row==maxRow && currentPoint.col==maxCol)
+			return 1;		//check current position is (a-1, b-1) which means end point.
+		
+		if (currentPoint.value==2)
+		{
+			//check if valid otherwise return 0;
+			if(currentPoint.col-1<0)
+				return 0;			
+
+			return North(board, board[currentPoint.row][currentPoint.col-1]);
+		}
+		else if(currentPoint.value==3)
+		{
+			if(currentPoint.row-1<0)
+				return 0;
+			
+			return North(board, board[currentPoint.row-1][currentPoint.col]);
+		}
+		else if(currentPoint.value==6)
+		{
+			if(currentPoint.row+1>maxRow)
+				return 0;
+			
+			return South(board, board[currentPoint.row+1][currentPoint.col]);
+		}
+		else return 0;
+	}
+	
+	public int East(Point [][]board, Point currentPoint)
+	{
+		if(currentPoint.row==maxRow && currentPoint.col==maxCol)
+			return 1;		//check current position is (a-1, b-1) which means end point.
+		
+		if (currentPoint.value==2)
+		{
+			//check if valid otherwise return 0;			
+			if(currentPoint.col+1<maxCol)
+				return 0;			
+
+			return East(board, board[currentPoint.row][currentPoint.col+1]);
+		}
+		else if(currentPoint.value==4)
+		{
+			if(currentPoint.row-1<0)
+				return 0;
+			
+			return North(board, board[currentPoint.row-1][currentPoint.col]);
+		}
+		else if(currentPoint.value==6)
+		{
+			if(currentPoint.row+1>maxRow)
+				return 0;
+			
+			return South(board, board[currentPoint.row+1][currentPoint.col]);
+		}
+		else return 0;
+	}
 }
+
--- cs252 Rotate Game/src/Point.java
+++ cs252 Rotate Game/src/Point.java
@@ -3,6 +3,9 @@ public class Point {
 	int row;
 	int col;
 	int mark=0;
+	int value = 0;	//pipe type  ranges from 1-6
+					//1: straight vertical, 2: straight horizontal, 3: bend up and right, 4: bend up and left, 
+					//5: bend down and left, 6: bend down right
 	
 	public Point (int a, int b, int c) {
 		row=a;
@@ -17,5 +20,9 @@ public class Point {
 	public int marked()
 	{
 		return mark;
+	}
+	public void setPointValue(int v)//sets the value (which pipe) the given point is
+	{
+		value=v;
 	}
 }
